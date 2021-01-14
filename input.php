af3ceed9edc9b79a7931131be96b033a1b5a48bc
I'am tubasya <56450913+Imtubasya@users.noreply.github.com>
2021-01-14 09:13:24 +0700

    input.php

--- input.php
+++ input.php
@@ -2,10 +2,10 @@
 
 /* INPUT USER-AGNET */
 
-$ugent="Mozilla/5.0 (Linux; Android 9; Mi A1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36";
+$ugent="";
 
 /* INPUT COOKIE */
 
-$cookie="PT_REF_ID=4267; AccExist=Iamtubasya; AutoLogin=ses_user%3DIamtubasya%26ses_hash%3D53cf195ae0e4f5523fc25bfa26e3ef82; PHPSESSID=in0rvakutuca529cqcmdepuph4";
+$cookie="";
 
 ?>
